4681fb359a92ab2e2a06cd2da5bff7bac4bfbcbf
darioelao <darioelao@gmail.com>
2023-02-01 22:24:39 -0500

    Inital files commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const homeRoutes = require('./routes/homeroutes');
+const apiRoutes = require('./routes/apiroutes')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use(apiRoutes)
+app.use(homeRoutes)
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
